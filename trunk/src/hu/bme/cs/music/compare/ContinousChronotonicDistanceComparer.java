e975106deb7f4213aac0a184f124137bcf26448d
nyitraijaska <nyitraijaska@38d73f11-a5c6-386a-73f1-048bc7a08f04>
2013-04-05 15:17:39 +0000

    review code and added some utilities

---
+++ trunk/src/hu/bme/cs/music/compare/ContinousChronotonicDistanceComparer.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package hu.bme.cs.music.compare;
+
+import hu.bme.cs.music.model.Comparer;
+import hu.bme.cs.music.model.Tune;
+import hu.bme.cs.music.model.TuneLine;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author Jozsef
+ * 
+ */
+public class ContinousChronotonicDistanceComparer extends Comparer {
+	
+	private double[][] distMx;
+
+	public ContinousChronotonicDistanceComparer(List<Tune> tunes) {
+		distMx = init(tunes);
+	}
+	
+	@Override
+	public String getName() {
+		return "Continos chronotonic distances";
+	}
+
+	@Override
+	public double[][] getDistanceMx() {
+		return distMx;
+	}
+
+
+	@Override
+	public double compare(TuneLine t1, TuneLine t2) {
+		List<Integer> chain1 = t1.getChronotonicChain();
+		List<Integer> chain2 = t2.getChronotonicChain();
+		List<Double> co1 = t1.getChronotonicCoordinates();
+		List<Double> co2 = t2.getChronotonicCoordinates();
+		int s1 = chain1.size();
+		int s2 = chain2.size();
+		double res = TuneLine.chronotonicResolution;
+		Set<Double> coords = new TreeSet<Double>(co1);
+		coords.addAll(co2);
+
+		double s = 0.0;
+		double prev = 0.0;
+		for (Double d : coords) {
+			int i1 = (int) Math.floor((d / res) * s1);
+			int i2 = (int) Math.floor((d / res) * s2);
+			if (co1.contains(d)) {
+				i1--;
+			}
+			if (co2.contains(d)) {
+				i2--;
+			}
+			double diff = Math.abs(chain1.get(i1) - chain2.get(i2));
+			s += diff * (d - prev);
+			prev = d;
+		}
+
+		double max = Collections.max(chain1) > Collections.max(chain2) ? Collections
+				.max(chain1) : Collections.max(chain2);
+
+		return s / (res * max);
+	}
+
+}
---
+++ trunk/src/hu/bme/cs/music/compare/SwapDistanceComparer.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package hu.bme.cs.music.compare;
+
+import hu.bme.cs.music.model.Comparer;
+import hu.bme.cs.music.model.Tune;
+import hu.bme.cs.music.model.TuneLine;
+
+import java.util.List;
+
+/**
+ * @author Jozsef
+ * 
+ */
+public class SwapDistanceComparer extends Comparer {
+	
+	private double[][] distMx;
+
+	public SwapDistanceComparer(List<Tune> tunes) {
+		distMx = init(tunes);
+	}
+	
+	@Override
+	public String getName() {
+		return "Swap distances";
+	}
+
+	@Override
+	public double[][] getDistanceMx() {
+		return distMx;
+	}
+
+	@Override
+	public double compare(TuneLine t1, TuneLine t2) {
+		List<Integer> e1 = t1.getOnsetVector();
+		List<Integer> e2 = t2.getOnsetVector();
+
+		int size1 = e1.size();
+		int size2 = e2.size();
+
+		double s = 0.0;
+
+		for (int i = 0; (i < size1 && i < size2); i++) {
+			s += Math.abs(e1.get(i) - e2.get(i));
+		}
+
+		return s;
+	}
+
+}
